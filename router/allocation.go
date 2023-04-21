0910b1cdc24753ccd31ae36a76a184b60c67be20
rey <3065588496@qq.com>
2023-04-21 10:08:25 +0800

    added ef1 router

--- router/allocation.go
+++ router/allocation.go
@@ -10,20 +10,10 @@ func SetUp() *gin.Engine {
 	router := gin.Default()
 
 	router.Use(middleware.Cors())
-	// ef1
+
+	// saveworld = ef1
 	router.POST("/saveworld", logic.SaveWorld)
+	router.POST("/ef1", logic.SaveWorld)
 
-	//v1Group := router.Group("/v1/api/user", middlewares.JWTAuth())
-	//{
-	//// info
-	//v1Group.GET("/info/:mobile", logic.InfoHandler)
-	//// update
-	//v1Group.PUT("/update", logic.UpdateHandler)
-	//// delete
-	//v1Group.DELETE("/delete/:mobile", logic.DeleteHandler)
-	//// re password
-	//v1Group.PUT("/repassword", logic.RePasswordHandler)
-
-	//}
 	return router
 }
